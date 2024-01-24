e5f0fe0baf1aa471d38e6950b7da72f2855059e1
theMackabu <theMackabu@gmail.com>
2024-01-23 22:29:34 -0800

    update readme

---
+++ README.md
@@ -0,0 +1,15 @@
+## discenage configuration
+
+```json5
+// book.json
+{
+	"pagesList": [
+	 {
+		"target": "content/cover.html",
+		"title": "Cover Page"
+	 },
+	 // add more as needed
+}
+```
+
+To get `cookie.json`, export using [this tool](https://chromewebstore.google.com/detail/copy-cookies/jcbpglbplpblnagieibnemmkiamekcdg)!
--- src/download.js
+++ src/download.js
@@ -7,7 +7,21 @@ export async function downloadPageAsPDF(page, browser, baseUrl, cookiesData) {
 		const url = baseUrl + page.target;
 
 		await pageInstance.setCookie(...cookiesData);
-		await pageInstance.goto(url, { waitUntil: 'domcontentloaded' });
+		await pageInstance.goto(url, { waitUntil: 'networkidle2' });
+
+		await pageInstance.evaluate(() => {
+			const summaryElements = document.querySelectorAll('details');
+			summaryElements.forEach((summary) => {
+				summary.setAttribute('open', 'open');
+			});
+		});
+
+		await pageInstance.evaluate(() => {
+			const summaryContentWrapperElements = document.querySelectorAll('.summary-content-wrapper');
+			summaryContentWrapperElements.forEach((wrapper) => {
+				wrapper.setAttribute('style', 'overflow: visible;');
+			});
+		});
 
 		const pdfPath = path.join(path.dirname(page.target), `${path.basename(page.target, '.html')}.pdf`);
 		await pageInstance.pdf({ path: pdfPath, format: 'A4' });
